1400f0e4f0c4973a2bfe69e0aa043087a9cd7277
monster <Toby@dhcp-168-105-83-22.wireless.manoa.hawaii.edu>
2014-01-05 17:34:18 -1000

    testing out this embedding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embeddr::Application.config.secret_key_base = 'a33f5e5078b8d8687d130715e063b6c6e2aa9791023cf1d7381f6f0dfd7494300a55edb43d14455007957607eb995ca97ddaf82bf4b5390989091f35d6e51b0e'
